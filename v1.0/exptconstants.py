2a526c7ff6108c9845eb3f3dd1f6abbb55d5f629
camilla-ip <camilla.ip@well.ox.ac.uk>
2016-11-03 00:42:58 +0000

    fixes to account for new fast5_extract fn args

--- v1.0/exptconstants.py
+++ v1.0/exptconstants.py
@@ -66,11 +66,11 @@ def Compute_Share(args, P, mylogger, myhandler, processname, exptidL, E):
       # Extract all metadata
         M = {}
         for fast5 in fast5L:
-             attributeD, runnumberD, readnumberD = P.fast5_attributes(fast5, 'all', True)
+             attributeD, runnumberD, readnumberD, fastqD = P.fast5_extract(fast5, E[exptid]['NNN'], True, True, False, True)
              #attrDbestN = Filter_Attributes(attributeD)
-             attrDbestN, filterok = P.fast5_attributes_filter(attributeD, E[exptid]['basecallN'])
+             attrDbestN, filterok = P.fast5_attributes_filter(attributeD, E[exptid]['NNN'])
              if not filterok:
-                 mylogger.error('Failed to filter basecallN in attributes, skipping file ({0})'.format(fast5))
+                 mylogger.error('Failed to filter NNN in attributes, skipping file ({0})'.format(fast5))
                  continue
              M[fast5] = attrDbestN
       # Keep the fields that are constant in this experiment C[exptid] = {var:val, ...}
