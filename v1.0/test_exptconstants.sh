a28742bec1763dbc880f87bbb81cb713fa44c856
camilla-ip <camilla.ip@well.ox.ac.uk>
2017-05-22 16:58:33 +0100

    Initial revision

--- v1.0/test_exptconstants.sh
+++
@@ -1,8 +0,0 @@
-python -m pdb \
-./marcoporo.py exptconstants \
--bin /well/bsg/microbial/marc/phase2/marcoporo/v1.0 \
--profile None \
--config /well/bsg/microbial/marc/phase2/marcp2/data/00-config/config.txt \
--experiments /well/bsg/microbial/marc/phase2/marcp2/data/00-config/experiments.txt \
--samplesize 300 \
--outdir /well/bsg/microbial/marc/phase2/marcp2/data/02-extract
